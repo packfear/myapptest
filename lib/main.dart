98794bf6e8fe4f779218f3e918733ccfa931c9d3
packfear <packfear@gmail.com>
2025-01-25 13:05:06 +0000

    modified:   android/app/build.gradle 	modified:   lib/main.dart 	modified:   lib/screens/signup.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:myapp/screens/chat_page.dart';
 import 'package:myapp/screens/signin_page.dart';
 import 'package:myapp/screens/signup.dart';
 import 'package:myapp/screens/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -12,46 +12,54 @@ class SingUpPage extends StatefulWidget {
 class _SingUpPageState extends State<SingUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 180, child: Image.asset('images/login.png')),
-            const SizedBox(
-              height: 30,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 50),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 180, child: Image.asset('images/login.png')),
+                const SizedBox(
+                  height: 30,
+                ),
+                                     MyTexfield(
+                        keyboardType: TextInputType.emailAddress,
+                        isabscured: false,
+                        hintText: 'Email',
+                        onChanged: (value) {},
+                      ),
+                      const SizedBox(
+                    height: 10,
+                  ),
+                  MyTexfield(
+                    keyboardType: TextInputType.text,
+                    isabscured: true,
+                    hintText: 'Password',
+                    onChanged: (value) {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyTexfield(
+                    keyboardType: TextInputType.text,
+                    isabscured: true,
+                    hintText: 'Repeat Password',
+                    onChanged: (value) {},
+                  ),
+                  const SizedBox(height: 10,),
+                  MyButton(color: Colors.blue[900]!, onPressed: (){}, text: 'Sign Up')
+                  
+                    
+                  
+               
+                
+              ],
             ),
-            MyTexfield(
-              keyboardType: TextInputType.emailAddress,
-              isabscured: false,
-              hintText: 'Email',
-              onChanged: (value) {},
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyTexfield(
-              keyboardType: TextInputType.text,
-              isabscured: true,
-              hintText: 'Password',
-              onChanged: (value) {},
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyTexfield(
-              keyboardType: TextInputType.text,
-              isabscured: true,
-              hintText: 'Repeat Password',
-              onChanged: (value) {},
-            ),
-            const SizedBox(height: 10,),
-            MyButton(color: Colors.blue[900]!, onPressed: (){}, text: 'Sign Up')
-
-          ],
+          ),
         ),
       ),
     );
