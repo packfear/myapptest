5a625b60ea67842056dc961e0ac22a5bf8b0d557
packfear <packfear@gmail.com>
2025-01-27 23:25:30 +0100

    modified:   lib/screens/signin_page.dart 	modified:   lib/screens/signup.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:myapp/widgets/mybutton.dart';
 import 'package:myapp/widgets/mytextfield.dart';
 
@@ -12,6 +13,8 @@ class SignInPage extends StatefulWidget {
 
 class _SignInPageState extends State<SignInPage> {
   final _auth = FirebaseAuth.instance;
+  bool _saving = false;
+
   late String email;
   late String password;
 
@@ -19,54 +22,63 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 180, child: Image.asset('images/login.png')),
-            const SizedBox(
-              height: 30,
-            ),
-            MyTexfield(
-              keyboardType: TextInputType.emailAddress,
-              isabscured: false,
-              hintText: 'Email',
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyTexfield(
-              keyboardType: TextInputType.text,
-              isabscured: true,
-              hintText: 'Password',
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyButton(
-                color: Colors.orange[900]!,
-                onPressed: () async {
-                  try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
-                    if (user != null) {
-                      Navigator.pushNamed(context, 'chat_page');
-                    }
-                  } on Exception catch (e) {
-                    // TODO
-                    print(e);
-                  }
+      body: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 180, child: Image.asset('images/login.png')),
+              const SizedBox(
+                height: 30,
+              ),
+              MyTexfield(
+                keyboardType: TextInputType.emailAddress,
+                isabscured: false,
+                hintText: 'Email',
+                onChanged: (value) {
+                  email = value;
                 },
-                text: 'Sign In')
-          ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTexfield(
+                keyboardType: TextInputType.text,
+                isabscured: true,
+                hintText: 'Password',
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyButton(
+                  color: Colors.orange[900]!,
+                  onPressed: () async {
+                    try {
+                      setState(() {
+                        _saving = true;
+                      });
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.pushNamed(context, 'chat_page');
+                        setState(() {
+                          _saving = false;
+                        });
+                      }
+                    } on Exception catch (e) {
+                      // TODO
+                      print(e);
+                    }
+                  },
+                  text: 'Sign In')
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:myapp/widgets/mybutton.dart';
 import 'package:myapp/widgets/mytextfield.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -12,6 +13,7 @@ class SingUpPage extends StatefulWidget {
 
 class _SingUpPageState extends State<SingUpPage> {
   final _auth = FirebaseAuth.instance;
+  bool _saving = false;
 
   late String email;
   late String password;
@@ -20,84 +22,93 @@ class _SingUpPageState extends State<SingUpPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 180, child: Image.asset('images/login.png')),
-                const SizedBox(
-                  height: 30,
-                ),
-                MyTexfield(
-                  keyboardType: TextInputType.emailAddress,
-                  isabscured: false,
-                  hintText: 'Email',
-                  onChanged: (value) {
-                    email = value;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                MyTexfield(
-                  keyboardType: TextInputType.text,
-                  isabscured: true,
-                  hintText: 'Password',
-                  onChanged: (value) {
-                    password = value;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                MyTexfield(
-                  keyboardType: TextInputType.text,
-                  isabscured: true,
-                  hintText: 'Repeat Password',
-                  onChanged: (value) {
-                    repeatPassword = value;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                MyButton(
-                    color: Colors.blue[900]!,
-                    onPressed: () async {
-                      if (email.isNotEmpty &&
-                          password.isNotEmpty &&
-                          repeatPassword.isNotEmpty) {
-                        try {
-                          if (password == repeatPassword) {
-                            final user =
-                                await _auth.createUserWithEmailAndPassword(
-                                    email: email, password: password);
-                            if (user != null) {
-                              Navigator.pushNamed(context, 'chat_page');
+      child: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 180, child: Image.asset('images/login.png')),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  MyTexfield(
+                    keyboardType: TextInputType.emailAddress,
+                    isabscured: false,
+                    hintText: 'Email',
+                    onChanged: (value) {
+                      email = value;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyTexfield(
+                    keyboardType: TextInputType.text,
+                    isabscured: true,
+                    hintText: 'Password',
+                    onChanged: (value) {
+                      password = value;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyTexfield(
+                    keyboardType: TextInputType.text,
+                    isabscured: true,
+                    hintText: 'Repeat Password',
+                    onChanged: (value) {
+                      repeatPassword = value;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyButton(
+                      color: Colors.blue[900]!,
+                      onPressed: () async {
+                        if (email.isNotEmpty &&
+                            password.isNotEmpty &&
+                            repeatPassword.isNotEmpty) {
+                          try {
+                            if (password == repeatPassword) {
+                              setState(() {
+                                _saving = true;
+                              });
+                              final user =
+                                  await _auth.createUserWithEmailAndPassword(
+                                      email: email, password: password);
+                              if (user != null) {
+                                Navigator.pushNamed(context, 'chat_page');
+                              }
+                              setState(() {
+                                _saving = false;
+                              });
                             }
+                          } catch (e) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('You have problem'),
+                              ),
+                            );
                           }
-                        } catch (e) {
+                        } else {
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
-                              content: Text('You have problem'),
+                              content: Text('Please fill all the fields'),
                             ),
                           );
                         }
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Please fill all the fields'),
-                          ),
-                        );
-                      }
-                    },
-                    text: 'Sign Up')
-              ],
+                      },
+                      text: 'Sign Up')
+                ],
+              ),
             ),
           ),
         ),
