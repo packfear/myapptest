90b52d08b65bc2158983de56740e8bf6ba856e10
packfear <packfear@gmail.com>
2025-01-29 22:39:26 +0000

    modified:   lib/widgets/message_line.dart

--- lib/widgets/message_line.dart
+++ lib/widgets/message_line.dart
@@ -24,19 +24,22 @@ class MessageLine extends StatelessWidget {
           ),
           Material(
               elevation: 5,
-              borderRadius: const BorderRadius.only(
+              borderRadius: isMe ?  const BorderRadius.only(
                 topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)) :  const BorderRadius.only(
+                topRight: Radius.circular(30),
                 bottomLeft: Radius.circular(30),
                 bottomRight: Radius.circular(30),
               ),
-              color: Colors.orange[700],
+              color: isMe ? Colors.orange[700] : Colors.blue[700],
               child: Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Text(
                   '$messageText',
-                  style: const TextStyle(
-                    fontSize: 15,
+                  style:  TextStyle(
+                    fontSize: 15, color: isMe ? Colors.black : Colors.white,
                   ),
                 ),
               )),
